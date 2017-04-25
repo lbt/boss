574ccd85c822ecbe6340275e25f5dfc4f7d5cc71
David Greaves <david@dgreaves.com>
2017-04-25 09:05:24 +0100

    Add some annotations in the BOSS reciever code
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- lib/boss/boss_receiver.rb
+++ lib/boss/boss_receiver.rb
@@ -1,6 +1,24 @@
 class BOSSReceiver < Ruote::Amqp::Receiver
 
-    #TODO: implement standard logger and error notifier instead of per process
-    # specific code
+  #TODO: implement standard logger and error notifier instead of per process
+  # specific code
+
+  def decode_message(header, payload)
+
+    item = super(header, payload)
+
+    if item['error'] && item['fei']
+        act = "that a participant has errored"
+      elsif item['fields'] && item['fei']
+        act = "to process workitem"
+      elsif item['process_definition'] || item['definition']
+        act = "to launch"
+      else
+        act = "something unknown - will now handle the error"
+    end
+    puts "Received amqp message: #{act}"
+
+    item
+  end
 end
 
